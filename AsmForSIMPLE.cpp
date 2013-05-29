31e4a474cc2325249732813051224ed815f29f09
okada <kokuzen@gmail.com>
2013-05-29 23:07:49 +0900

    modify to exit app safely with help message

--- AsmForSIMPLE.cpp
+++ AsmForSIMPLE.cpp
@@ -9,6 +9,7 @@ int main(int argc, char* argv[])
 	AsmForSIMPLE afs;
 	if(argc != 1 && argc != 3){
 		std::cout << "usage:\n\tno argument -> run interactive shell\n\t<src> <dst> -> compile src file and write it dst file." << std::endl;	
+		return 0;
 	}
 	if(argc==1){
 		afs.StartInteractiveShell(std::cin, "save");
