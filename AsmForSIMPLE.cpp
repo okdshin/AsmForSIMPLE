c3a0d223a8a0c6d8a65156b150c2e87122678923
okada <kokuzen@gmail.com>
2013-05-29 23:03:42 +0900

    cut some verbose codes

--- AsmForSIMPLE.cpp
+++ AsmForSIMPLE.cpp
@@ -8,16 +8,14 @@ int main(int argc, char* argv[])
 {
 	AsmForSIMPLE afs;
 	if(argc != 1 && argc != 3){
-		std::cout << "usage" << std::endl;	
+		std::cout << "usage:\n\tno argument -> run interactive shell\n\t<src> <dst> -> compile src file and write it dst file." << std::endl;	
 	}
 	if(argc==1){
 		afs.StartInteractiveShell(std::cin, "save");
 	}
 	else {
-		std::string src_file_name(argv[1]);
-		std::string dst_file_name(argv[2]);
-		std::ifstream src_file(src_file_name.c_str());
-		std::ofstream dst_file(dst_file_name.c_str());
+		std::ifstream src_file(argv[1]);
+		std::ofstream dst_file(argv[2]);
 		afs.CompileAsmFile(src_file, dst_file);
     }
 	return 0;
